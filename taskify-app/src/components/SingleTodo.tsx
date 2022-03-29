9533eb15240d7516f3003cfcf0c31bba72780d98
Ankur Chaudhary <ankurpat3194@gmail.com>
2022-03-30 00:04:42 +0530

    all functionality deployed

--- taskify-app/src/components/SingleTodo.tsx
+++ taskify-app/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -10,6 +10,14 @@ interface Props {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [canEdit, setCanEdit] = useState(false);
+  const [editTodo, setEditTodo] = useState(todo.todo);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [canEdit]);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) => {
@@ -26,16 +34,42 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     );
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, todo: editTodo } : todo;
+      })
+    );
+    setCanEdit(false);
+  };
+
   return (
-    <form className="todos__single">
-      {todo.isDone ? (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {canEdit ? (
+        <input
+          value={editTodo}
+          ref={inputRef}
+          onChange={(e) => {
+            setEditTodo(e.target.value);
+          }}
+          className="todos__single--text"
+        />
+      ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
       ) : (
         <span className="todos__single--text">{todo.todo}</span>
       )}
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!canEdit && !todo.isDone) {
+              setCanEdit(!canEdit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
         <span
